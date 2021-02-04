bb6e12cdb7a72bf44e1458e49215b304df62b550
spsarras <psarras.st@gmail.com>
2021-02-04 11:56:24 +0000

    Rhino object selector fires event when you click a RhinoObject

--- Packages/Unity.RhinoLoader/Runtime/RhinoObjectData.cs
+++ Packages/Unity.RhinoLoader/Runtime/RhinoObjectData.cs
@@ -4,18 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using Rhino.FileIO;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RhinoObjectData : MonoBehaviour
 {
     public File3dmObject File3dmObject { get; private set; }
-    [SerializeField] private List<ListStrings> Values;
+    public List<ListStrings> Values;
 
     public void Bind(File3dmObject contextFile3dmObject)
     {
         File3dmObject = contextFile3dmObject;
-        //Keys = new List<string>();
         Values = new List<ListStrings>();
-        //ValuesStrings = new List<string>();
 
         if (contextFile3dmObject.Attributes.UserStringCount == 0)
             return;
---
+++ Packages/Unity.RhinoLoader/Runtime/RhinoObjectSelector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using BitsNBobs.Singleton;
+using UnityEngine;
+
+public class RhinoObjectSelector : Singleton<RhinoObjectSelector>
+{
+    private Camera cam;
+    [SerializeField] private LayerMask layerMask;
+    public RhinoObjectData RhinoObjectData;
+    public event Action<RhinoObjectData> OnUpdated;
+    private void Awake()
+    {
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out RaycastHit hitInfo, 99999,
+                layerMask))
+            {
+                // Select
+                RhinoObjectData = hitInfo.collider.GetComponent<RhinoObjectData>();
+                OnUpdated?.Invoke(RhinoObjectData);
+                Debug.Log(hitInfo.collider.name);
+            }
+            else
+            {
+                // Deselect
+                RhinoObjectData = null;
+                OnUpdated?.Invoke(RhinoObjectData);
+            }
+        }
+    }
+}
